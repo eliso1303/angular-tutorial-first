1c2b5d5ea7a2e492eb30c0bc55d630c5bccf47b4
Elisabed Papashvili <oboe1303@gmail.com>
2019-07-10 20:48:44 +0400

    fix(Observable task-2): Fix task-2

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -16,6 +16,8 @@ export class ExchangeComponent implements OnInit {
   currency1Value: number;
   currency2Value: number;
   Observer;
+  checker1;
+  checker2;
   constructor(private currencyService: CurrencyService, private http: HttpClient) {
     this.currencies = currencyService.getCurrencies();
     this.Observer = new Observable(this.change);
@@ -40,31 +42,39 @@ export class ExchangeComponent implements OnInit {
     this.currency2Value = value;
   }
 
+  firstInput() {
+    this.checker1 = true;
+    this.checker2 = false;
+  }
+
+  secondInput() {
+    this.checker1 = false;
+    this.checker2 = true;
+  }
+
   change() {
     if (this.currency1 && this.currency2) {
-      const url = `https://api.exchangeratesapi.io/latest?base=${this.currency1}&symbols=${this.currency2}`;
-      this.http.get(url).subscribe(value => {
-        this.updateCurrency2Value(value);
-      });
-    }
-  }
-
-  updateCurrency2Value(value) {
-    let currencyValue = +Object.values(value.rates);
-    this.currency2Value = this.currency1Value * currencyValue;
-  }
-
-  changeFromCur2() {
-    if (this.currency1 && this.currency2) {
-      const url = `https://api.exchangeratesapi.io/latest?base=${this.currency2}&symbols=${this.currency1}`;
-      this.http.get(url).subscribe(value => {
-        this.updateCurrency1Value(value);
-      });
+      if (this.checker1) {
+        const url = `https://api.exchangeratesapi.io/latest?base=${this.currency1}&symbols=${this.currency2}`;
+        this.http.get(url).subscribe(value => {
+          this.updateCurrency2Value(value);
+        });
+      } else if (this.checker2) {
+        const url = `https://api.exchangeratesapi.io/latest?base=${this.currency2}&symbols=${this.currency1}`;
+        this.http.get(url).subscribe(value => {
+          this.updateCurrency1Value(value);
+        });
+      }
     }
   }
 
   updateCurrency1Value(value) {
     let currencyValue = +Object.values(value.rates);
     this.currency1Value = this.currency2Value * currencyValue;
+  }
+
+  updateCurrency2Value(value) {
+    let currencyValue = +Object.values(value.rates);
+    this.currency2Value = this.currency1Value * currencyValue;
   }
 }
