372f9db6c44e67c43938bb12740039abd3bc85ba
Elisabed Papashvili <oboe1303@gmail.com>
2019-07-10 14:37:34 +0400

    fix(Observable task-1): In progress

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CurrencyService } from './../currency.service';
+import { FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-exchange',
@@ -9,14 +13,44 @@ import { CurrencyService } from './../currency.service';
 export class ExchangeComponent implements OnInit {
   currencies;
   currency1;
-  constructor(private currencyService: CurrencyService) {
+  currency2;
+  currency1Value:number;
+  currency2Value:number;
+  Observer;
+  rates;
+  constructor(private currencyService: CurrencyService,
+    private http: HttpClient,
+    private formBuilder: FormBuilder) {
     this.currencies = currencyService.getCurrencies();
+    this.Observer = new Observable(this.change);
   }
 
   ngOnInit() {
   }
 
   onChange(value) {
-    console.log(value);
+    this.currency1 = value;
+  }
+
+  onKeyUp(value) {
+    this.currency1Value = value;
+  }
+
+  onChange2(value) {
+    this.currency2 = value;
+  }
+
+  change() {
+    console.log(this.currency1 +' '+this.currency1Value+' '+this.currency2);
+    if (this.currency1 && this.currency2) {
+      const url = ` https://api.exchangeratesapi.io/latest?base=${this.currency1}&symbols=${this.currency2}`;
+
+      this.http.get(url).subscribe(value => {
+        this.updateCurrency2Value(value);
+      });
+    }
+  }
+  updateCurrency2Value(value) {
+    this.currency2Value = 10;
   }
 }
