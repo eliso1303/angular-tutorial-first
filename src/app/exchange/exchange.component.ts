da0a56e6e6ecec30ecb08000ebc552fccc706e57
Elisabed Papashvili <oboe1303@gmail.com>
2019-07-10 17:40:41 +0400

    fix(Observable task-2): Finish task-2

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -36,6 +36,10 @@ export class ExchangeComponent implements OnInit {
     this.currency2 = value;
   }
 
+  onKeyUp2(value) {
+    this.currency2Value = value;
+  }
+
   change() {
     if (this.currency1 && this.currency2) {
       const url = `https://api.exchangeratesapi.io/latest?base=${this.currency1}&symbols=${this.currency2}`;
@@ -48,5 +52,19 @@ export class ExchangeComponent implements OnInit {
   updateCurrency2Value(value) {
     let currencyValue = +Object.values(value.rates);
     this.currency2Value = this.currency1Value * currencyValue;
+  }
+
+  changeFromCur2() {
+    if (this.currency1 && this.currency2) {
+      const url = `https://api.exchangeratesapi.io/latest?base=${this.currency2}&symbols=${this.currency1}`;
+      this.http.get(url).subscribe(value => {
+        this.updateCurrency1Value(value);
+      });
+    }
+  }
+
+  updateCurrency1Value(value) {
+    let currencyValue = +Object.values(value.rates);
+    this.currency1Value = this.currency2Value * currencyValue;
   }
 }
