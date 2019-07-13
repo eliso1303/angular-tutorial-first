f6b81b8bd6ee465a1619a96b6dcff00a2bfacdfc
Elisabed Papashvili <oboe1303@gmail.com>
2019-07-13 18:03:11 +0400

    feat(Http Tasks): Finish Task-5 and some styles

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeesService } from '../employees.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-employees',
@@ -8,11 +9,30 @@ import { EmployeesService } from '../employees.service';
 })
 export class EmployeesComponent implements OnInit {
 
-  employees$;
+  employees;
+  partOfEmployees;
+  allPagesNum = 0;
+  current = 0;
+  pages;
 
-  constructor(private employeesService: EmployeesService){}
+  constructor(private employeesService: EmployeesService, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
-    this.employees$ = this.employeesService.getEmployees();
+    this.employeesService.getEmployees().subscribe(element => {
+      this.employees = element;
+      this.partOfEmployees = this.employees.slice(this.current * 10, 10);
+      this.allPagesNum = Math.ceil(this.employees.length / 10);
+      this.pages = Array(this.allPagesNum).fill(0).map((x, i) => i);
+
+      this.route.queryParams.subscribe(value => {
+        if (value.page) {
+          this.current = value.page;
+        } else {
+          this.current = this.current;
+        }
+        this.partOfEmployees = this.employees.slice(this.current * 10, this.current * 10 + 10);
+      });
+    });
   }
 }
